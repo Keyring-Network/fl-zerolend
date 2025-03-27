49359bea0bbab94d37914ad964d8d316e3818efc
Florian de Miramon <florian@idm.io>
2025-03-27 11:53:06 +0100

    Misc

--- src/LeveragePositionManager.sol
+++ src/LeveragePositionManager.sol
@@ -8,12 +8,27 @@ import {IERC20Detailed as IERC20} from "@zerolend/dependencies/openzeppelin/cont
 import {AToken} from "@zerolend/protocol/tokenization/AToken.sol";
 import {IScaledBalanceToken} from "@zerolend/interfaces/IScaledBalanceToken.sol";
 
+
+/// For a brand new position (0 collateral, 0 borrowed), given a LTV, a user with an amount X of tokens can take the following position:
+    /// - Leverage factor = 1 / (1 - LTV)
+    /// - Total lent = X * leverage factor
+    /// - Total borrowed = X * leverage factor * LTV
+    ///
+    /// EG with LTV = 0.8 and X = 1000 USD:
+    /// - Leverage factor = 1 / (1 - 0.8) = 5
+    /// - Total lent = 1000 * 5 = 5000 USD
+    /// - Total borrowed = 5000 * 0.8 = 4000 USD
+    /// The X amount covers the difference between the total lent and the total borrowed.
+
+
 contract LeveragePositionManager {
     error TokenNotSupported(address tokenAddress);
     error InsufficientLentTokenBalance(address tokenAddress, uint256 balance);
     error LTVTooHigh(uint256 requestedLTV, uint256 maxPoolLTV);
     IPoolAddressesProviderRegistry public immutable poolAddressesProviderRegistry;
 
+
+    
     constructor(address _poolAddressesProviderRegistry) {
         poolAddressesProviderRegistry = IPoolAddressesProviderRegistry(_poolAddressesProviderRegistry);
     }
@@ -38,26 +53,14 @@ contract LeveragePositionManager {
         }
     }
 
-    function getMaxEthToSupplyForMaxLTV(address user) public view returns (uint256) {
-        // IPool pool = aLentToken.POOL();
-        // (uint256 totalCollateralBase, uint256 totalDebtBase, uint256 availableBorrowsBase,) =
-        //     pool.getUserAccountData(user);
-
-        // uint256 maxLTV = pool.getConfiguration(address(aLentToken)).getLtv();
-        // uint256 ltv = pool.getLtv(address(aLentToken));
-        // uint256 maxLTV = pool.MAX_LTV();
-        // uint256 ltv = pool.getLtv(address(aLentToken));
-        // uint256 maxLTV = pool.MAX_LTV();
-        // uint256 ltv = pool.getLtv(address(aLentToken));
-    }
-
     // @param aLentToken: aToken of the lent token
     // @param aBorrowedToken: aToken of the borrowed token
-    // @param interestRateMode: true if the interest rate mode is variable, false otherwise: Mode stable (MODE 1) or Mode variable (MODE 2)
+    // @param interestRateMode: Mode of the interest rate: Mode stable (MODE 1) or Mode variable (MODE 2)
+    // @param targetLtv: Target LTV to reach with a mantissa of 10000. Leave empty to use the max LTV of the pool
     // @notice: args expecting ATokens to be able to check their existence in the protocol
     // @notice: this function is used to take a leveraged position by looping lending and borrowing using the same token
     // @notice: not all prerequisites are checked here (pool liquidity availability, token used as collateral, IRMode, etc.) as it will be called by the core contracts. Only basic prerequisites are checked here (token user's balance and ltv limit to have early revert)
-    function takePositionByLentTokenAmount(AToken aToken, uint256 lentTokenAmount, bool interestRateMode) public {
+    function takePosition(AToken aToken, uint256 lentTokenAmount, uint256 targetLtv, bool interestRateMode) public {
 
         // @dev: revert if the token is not supported
         revertIfATokenNotSupported(aToken);
@@ -67,92 +70,51 @@ contract LeveragePositionManager {
             revert InsufficientLentTokenBalance(address(aLentToken), lentTokenAmount);
         }
 
-       // @dev: get the pool address
         IPool pool = aToken.POOL();
 
-        // @dev: get the ltv
         uint256 maxLTV = pool.MAX_LTV();
-        
-        // @dev: get the resulting ltv for this lent token amount
-        uint256 inferedLtv = _getLTVToTargetForThisLentTokenAmount(aToken, msg.sender, lentTokenAmount);
 
-        if (inferedLtv > maxLTV) {
-            revert LTVTooHigh(inferedLtv, maxLTV);
+        if (targetLtv > maxLTV) {
+            revert LtvTooHigh(targetLtv, maxLTV);
         }
 
-        
+        targetLtv = targetLtv == 0 ? maxLTV : targetLtv;
+
+        uint256 currentLtv = pool.getUserAccountData(user).ltv;
+
+        if (currentLtv == targetLtv) {
+            revert LtvTooLow(targetLtv, currentLtv);
+        }
+
+        // @dev: get the current collateral in base
+        uint256 totalCollateralBase = pool.getUserAccountData(user).totalCollateralBase;
+
+        // @dev: get the target collateral in base. This is the lentTokenAmount x the leverage factor
+        uint256 targetCollateralBase = lentTokenAmount / (10000 - targetLtv) * 10000;
+
+        // @dev: get the amount of collateral to get from flashloan
+        uint256 collateralToGetFromFlashloanBase = targetCollateralBase - totalCollateralBase;
+
+        // @dev: get the price of the token in base currency
+        // @notice: Eth mantissa is 18. `getAssetPrice` mantissa is 18 as well
+        // @notice: eg: 
+            // - 1 ETH (base currency) = 2000 USD
+            // - 1 BTC = 80000 USD
+            // => getAssetPrice(BTC) = 80000 / 2000 x 10^18 = 4000000000000000000000000
+        uint256 tokenPrice = pool.getPriceOracle().getAssetPrice(address(aToken.UNDERLYING()));
+        uint256 collateralToGetFromFlashloanInToken = collateralToGetFromFlashloanBase / tokenPrice * 1e18;
+
         // @dev: safe transfer the lent token to the contract
-        // aLentToken.underlying().safeTransferFrom(msg.sender, address(this), lentTokenAmount);
+        aLentToken.underlying().safeTransferFrom(msg.sender, address(this), lentTokenAmount);
 
-        // // @dev: get the pool address
+        // @dev: prepare the flashloan
+        // @notice: we need to approve the pool to spend the collateral token
+        aLentToken.underlying().approve(address(pool), collateralToGetFromFlashloanInToken);
 
-        // // @dev: approve the lent token to be spent by the pool
-        // aLentToken.approve(address(pool), lentTokenAmount);
+        // @dev: execute the flashloan
+        pool.flashLoan(address(this), address(aLentToken.underlying()), collateralToGetFromFlashloanInToken, "");
+        
 
-        // // @dev: deposit the lent token into the pool
-        // pool.deposit(address(this), lentTokenAmount, msg.sender, 0);
-
-        // uint256 lentTokenAmount = lentToken.balanceOf(address(this));
-        // lentToken.approve(address(pool), lentTokenAmount);
-        // pool.deposit(address(this), lentTokenAmount, msg.sender, 0);
     }
 
-
-
-    //***
-    // INTERNAL FUNCTIONS - VIEWERS
-    //***
-
-
-    // @param aToken: aToken to check if it is supported
-    // @param ltv: ltv to check. Leave it empty if you want to stick to the max ltv
-    // @notice: returns the amount of lent token needed to reach the requested ltv
-    // @notice: for the sake of simplicity, the premium for the flashloan is not assessed here. Hence the actual ltv might be lower than expected when the position is taken
-    function _getLentTokenAmountToTargetForThisLTV(AToken aToken, address user, uint256 ltv) internal view returns (uint256) {
-
-        revertIfATokenNotSupported(aToken);
-
-        // @dev: get the pool address
-        IPool pool = aToken.POOL();
-
-        // @dev: get the ltv
-        uint256 maxLTV = pool.MAX_LTV();
-
-        if (ltv > maxLTV) {
-            revert LTVTooHigh(ltv, maxLTV);
-        }
-
-        if (ltv == 0) {
-            ltv = maxLTV;
-        }
-
-        // @dev: get the amount of available borrows in base
-        uint256 availableBorrowsBase = pool.getUserAccountData(user).availableBorrowsBase;
-
-        // @dev: get the price of the token
-        uint256 tokenPrice = priceOracle.getAssetPrice(address(aToken.UNDERLYING()));
-
-        // @dev: get the amount of available borrows in token
-        uint256 availableBorrowsInToken = tokenPrice * availableBorrowsBase;
-
-        // @dev: get the amount of lent token to target to reach the ltv
-        uint256 inferedTokenAmount = (availableBorrowsInToken * ltv) / (10000 * (10 ** aToken.DECIMALS()));
-
-        return inferedTokenAmount;
-    
-    }
-
-
-    // @param aToken: aToken to check if it is supported
-    // @param user: user to check
-    // @param lentTokenAmount: amount of lent token to check
-    // @notice: returns the ltv that will be reached for this lent token amount
-    // @notice: for the sake of simplicity, the premium for the flashloan is not assessed here. Hence the actual ltv might be lower than expected when the position is taken
-    function _getLTVToTargetForThisLentTokenAmount(AToken aToken, address user, uint256 lentTokenAmount) internal view returns (uint256) {
-
-        revertIfATokenNotSupported(aToken);
-
-        // @dev: get the pool address
-        IPool pool = aToken.POOL();
-    }
 }
