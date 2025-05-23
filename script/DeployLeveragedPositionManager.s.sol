8eba97ae76d7880e04eb5fe160184e62afc53d2c
Sahil-Gujrati <sahilgujrati12@gmail.com>
2025-05-23 15:59:59 +0530

    chore: Update deployment script

--- script/DeployLeveragedPositionManager.s.sol
+++ script/DeployLeveragedPositionManager.s.sol
@@ -6,10 +6,12 @@ import {Script} from "forge-std/Script.sol";
 import {LeveragedPositionManager} from "@src/LeveragedPositionManager.sol";
 
 contract DeployLeveragedPositionManager is Script {
+    address private uniswapV2Factory;
     address private owner;
     uint16 private feeInBps;
 
     function setUp() public {
+        uniswapV2Factory = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
         owner = msg.sender;
         feeInBps = 50;
     }
@@ -17,7 +19,8 @@ contract DeployLeveragedPositionManager is Script {
     function run() public returns (LeveragedPositionManager) {
         vm.startBroadcast();
 
-        LeveragedPositionManager leveragedPositionManager = new LeveragedPositionManager(owner, feeInBps);
+        LeveragedPositionManager leveragedPositionManager =
+            new LeveragedPositionManager(uniswapV2Factory, owner, feeInBps);
 
         vm.stopBroadcast();
 
